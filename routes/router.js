d717319975979482dc927690c43031e79868a856
carfiguru@hotmail.com <carfiguru@hotmail.com>
2022-08-31 21:00:55 -0500

    debugged server issue

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const apiUserRoutes = require('./api-router/user-routes');
-const apiThoughtsRoutes = require('./api-router/thought-routes');
+const apiUserRoutes = require('./api-router/user-routes.js');
+const apiThoughtsRoutes = require('./api-router/throught-routes.js');
 
 router.use('/api/users', apiUserRoutes);
 router.use('/api/thoughts', apiThoughtsRoutes);
